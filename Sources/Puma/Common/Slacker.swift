3f22f86a25dec99d8f9c061a1ba035c4f0d37fa9
besarismaili <b3sar11@gmail.com>
2019-08-24 00:04:06 +0200

    slacker test

--- Sources/Puma/Common/Slacker.swift
+++ Sources/Puma/Common/Slacker.swift
@@ -14,8 +14,11 @@ public struct Slacker {
         let bot = SlackKit()
         let oauthConfig = OAuthConfig(clientID: "721702835698.738597109911", clientSecret: "01ae99d7df21b4ff1f9f5014340f9a07")
         bot.addServer(oauth: oauthConfig)
-        let helloAction = Action(name: "hello", text: "🌎")
-        let attachment = Attachment(fallback: "Hello World", title: message, callbackID: "hello_world", actions: [helloAction])
-        bot.webAPI?.sendMessage(channel: "slackapptest", text: "", attachments: [attachment], success: nil, failure: nil)
+        bot.notificationForEvent(.message) { (event, _) in
+            print(event.message)
+        }
+//        let helloAction = Action(name: "hello", text: "🌎")
+//        let attachment = Attachment(fallback: "Hello World", title: message, callbackID: "hello_world", actions: [helloAction])
+//        bot.webAPI?.sendMessage(channel: "slackapptest", text: "", attachments: [attachment], success: nil, failure: nil)
     }
 }
